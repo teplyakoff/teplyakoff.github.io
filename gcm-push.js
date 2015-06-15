63d5c7ebf69b1b6f3c8cbcc4b599786426ca0ca8
Dmitry Teplyakov <dmitry.teplyakov@together.com>
2015-06-15 18:34:12 +0300

    check permission

--- gcm-push.js
+++ gcm-push.js
@@ -14,6 +14,62 @@ function subscribe(pushSubscription)
                         resolve(pushSubscription);
                     })
                     .catch(function (e) {
+                        // Check for a permission prompt issue
+                        if ('permissions' in navigator) {
+                            navigator.permissions.query({name: 'push', userVisibleOnly: true})
+                                .then(function (permissionStatus) {
+                                    console.log('subscribe() Error: Push permission status = ',
+                                        permissionStatus);
+                                    if (permissionStatus.status === 'denied') {
+                                        // The user blocked the permission prompt
+                                        console.log('Ooops Notifications are Blocked',
+                                            'Unfortunately you just permanently blocked notifications. ' +
+                                            'Please unblock / allow them to switch on push ' +
+                                            'notifications.');
+                                    } else if (permissionStatus.status === 'prompt') {
+                                        console.log('The user didn\'t accept the permission prompt');
+                                    } else {
+                                        console.log('Ooops Push Couldn\'t Register',
+                                            '<p>When we tried to ' +
+                                            'get the subscription ID for GCM, something went wrong,' +
+                                            ' not sure why.</p>' +
+                                            '<p>Have you defined "gcm_sender_id" and ' +
+                                            '"gcm_user_visible_only" in the manifest?</p>' +
+                                            '<p>Error message: ' +
+                                            e.message +
+                                            '</p>');
+                                    }
+                                    reject(e);
+                                }).catch(function (err) {
+                                    console.log('Ooops Push Couldn\'t Register',
+                                        '<p>When we tried to ' +
+                                        'get the subscription ID for GCM, something went wrong, not ' +
+                                        'sure why.</p>' +
+                                        '<p>Have you defined "gcm_sender_id" and ' +
+                                        '"gcm_user_visible_only" in the manifest?</p>' +
+                                        '<p>Error message: ' +
+                                        err.message +
+                                        '</p>');
+                                    reject(err);
+                                });
+                        } else {
+                            // Use notification permission to do something
+                            if (Notification.permission === 'denied') {
+                                console.log('Ooops Notifications are Blocked',
+                                    'Unfortunately you just permanently blocked notifications. ' +
+                                    'Please unblock / allow them to switch on push notifications.');
+                            } else {
+                                console.log('Ooops Push Couldn\'t Register',
+                                    '<p>When we tried to ' +
+                                    'get the subscription ID for GCM, something went wrong, not ' +
+                                    'sure why.</p>' +
+                                    '<p>Have you defined "gcm_sender_id" and ' +
+                                    '"gcm_user_visible_only" in the manifest?</p>' +
+                                    '<p>Error message: ' +
+                                    e.message +
+                                    '</p>');
+                            }
+                        }
                         reject(e);
                     });
             });
